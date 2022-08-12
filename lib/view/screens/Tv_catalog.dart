b69f7ae3c01fcabf3fa210ad5aee26bad1607575
Kareem Ayman <Kareem.ayman1622@gmail.com>
2022-08-12 22:31:21 +0200

    Update Tv_catalog.dart

--- lib/view/screens/Tv_catalog.dart
+++ lib/view/screens/Tv_catalog.dart
@@ -2,7 +2,55 @@ import 'package:flutter/material.dart';
 import 'package:odcflutterapp1/model/tv.dart';
 
 class TvCatalog extends StatelessWidget {
-  List<Tv> flowers = [
+  List<Tv> tvs = [
+    Tv(
+        imageAddress:
+            "https://m.media-amazon.com/images/I/61DOcUmc0EL._AC_SX679_.jpg",
+        name: "Samsung 43 Inch Full HD Smart LED TV",
+        price: 6818,
+        brand: "Samsung",
+        desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"),
+    Tv(
+        imageAddress:
+            "https://m.media-amazon.com/images/I/71Oy86VIHbL._AC_SX679_.jpg",
+        name: "LG LED Smart TV 43 inch LM6370 Series Full HD HDR",
+        price: 6399,
+        brand: "LG",
+        desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"),
+    Tv(
+        imageAddress:
+            "https://m.media-amazon.com/images/I/71ePz00p-kL._AC_SX679_.jpg",
+        name: "Toshiba 4K Smart Frameless D-LED Ultra HD 65 Inch TV",
+        price: 11446,
+        brand: "Toshiba",
+        desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"),
+    Tv(
+        imageAddress:
+            "https://m.media-amazon.com/images/I/51-PKfOZ14L._AC_SX679_.jpg",
+        name: "Tornado 55 Inch 4K UHD Smart Android Frameless LED TV",
+        price: 6818,
+        brand: "Samsung",
+        desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"),
+    Tv(
+        imageAddress:
+            "https://m.media-amazon.com/images/I/61DOcUmc0EL._AC_SX679_.jpg",
+        name: "Samsung 43 Inch Full HD Smart LED TV",
+        price: 6818,
+        brand: "Samsung",
+        desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"),
+    Tv(
+        imageAddress:
+            "https://m.media-amazon.com/images/I/61DOcUmc0EL._AC_SX679_.jpg",
+        name: "Samsung 43 Inch Full HD Smart LED TV",
+        price: 6818,
+        brand: "Samsung",
+        desc:
+            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley"),
     Tv(
         imageAddress:
             "https://m.media-amazon.com/images/I/61DOcUmc0EL._AC_SX679_.jpg",
@@ -45,16 +93,16 @@ class TvCatalog extends StatelessWidget {
         leadingWidth: 150,
         // flexibleSpace: ,
         // toolbarOpacity: 2,
-        actions: [
+        actions: const [
           Padding(
-            padding: const EdgeInsets.only(
-              top: 15,
-              right: 15,
-            ),
-            child: Image.network(
-              "https://assets.webiconspng.com/uploads/2017/09/Sunflower-PNG-Image-54450.png",
-            ),
-          ),
+              padding: EdgeInsets.only(
+                top: 15,
+                right: 15,
+              ),
+              child: Icon(
+                Icons.shopping_cart,
+                color: Colors.grey,
+              )),
         ],
       ),
       body: Padding(
@@ -67,27 +115,76 @@ class TvCatalog extends StatelessWidget {
             crossAxisCount: 2,
             crossAxisSpacing: 20,
             mainAxisSpacing: 20,
+            childAspectRatio: 0.7,
           ),
-          itemCount: 10,
+          itemCount: tvs.length,
           itemBuilder: (BuildContext context, int index) {
             return Container(
               // height: 140,
               // width: 100,
-              padding: EdgeInsets.only(
+              padding: const EdgeInsets.only(
+                top: 10,
                 right: 10,
                 left: 10,
-                top: 10,
               ),
               decoration: BoxDecoration(
-                  color: Colors.blueAccent,
+                  color: Colors.grey.shade200,
                   borderRadius: BorderRadius.circular(20)),
               child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Image.network(
-                    flowers[0].imageAddress,
-                    fit: BoxFit.fitWidth,
+                  Container(
+                    height: 120,
+                    decoration: const BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.only(
+                          topLeft: Radius.circular(10),
+                          topRight: Radius.circular(10),
+                        )),
+                    child: Image.network(
+                      tvs[index].imageAddress,
+                      fit: BoxFit.fitWidth,
+                    ),
                   ),
-                  
+                  const SizedBox(
+                    height: 15,
+                  ),
+                  Text(
+                    "${tvs[index].name.substring(0, 15)}...",
+                    style: const TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 15,
+                    ),
+                  ),
+                  Text(
+                    "${tvs[index].brand}",
+                    style: const TextStyle(
+                      // fontWeight: FontWeight.w600,
+                      fontSize: 12,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 6,
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        "\$",
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 255, 17, 0),
+                        ),
+                      ),
+                      SizedBox(
+                        width: 2,
+                      ),
+                      Text(
+                        tvs[index].price.toString(),
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                        ),
+                      )
+                    ],
+                  ),
                   // SizedBox(
                   //   height: 10,
                   // ),
